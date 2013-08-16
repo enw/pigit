b3b186e47a030c1a90c339159b175c1bebb44d86
Elliot Winard <github@caveteen.com>
2013-08-16 13:26:38 -0400

    add pigit-specific stuff

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,8 +3,8 @@
 angular.module('pigitApp')
   .controller('MainCtrl', function ($scope) {
     $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+      'Nothing',
+      'Yet',
+      'But something is coming soon!'
     ];
   });
